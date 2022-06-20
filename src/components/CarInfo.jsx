39510bb7036872e62f4acdcd19acb0f0ee08552c
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-06-20 10:46:46 -0500

    cars page is half done

--- src/components/CarInfo.jsx
+++ src/components/CarInfo.jsx
@@ -10,11 +10,11 @@ const CarInfo = () => {
       <ul>Price: $40,000</ul>
       <ul>Gas Milage : 18 City/22 Highway</ul>
     </div>
-    <div className="mustang">
-    <h1> Ford Mustang</h1>
-    <ul>Model: EcoBoostPremium / GT</ul>
-    <ul>Price: $32,495 / $37,545 </ul>
-    <ul>Gas Milage: 21City/30Highway / 15City/24Highway</ul>
+    <div className="camry">
+        <h1>Toyota Camry</h1>
+        <ul>Model: TRD</ul>
+        <ul>Price: $32,910</ul>
+        <ul>Gas Milage: 21City/31Highway</ul>
     </div>
     <div className="subaru">
         <h1>Subaru</h1>
@@ -22,17 +22,35 @@ const CarInfo = () => {
         <ul>Price: $34,155</ul>
         <ul>Gas Milage: 19City/26Highway</ul>
     </div>
-    <div className="dodge">
-        <h1>Dodge Challenger</h1>
-        <ul>Model: GT / RT</ul>
-        <ul>Price: $33,800 / $39,075</ul>
-        <ul>Gas Milage: 19City/30Highway / 15City/23Highway</ul>
+    <div className="mustangV6">
+    <h1> Ford Mustang</h1>
+    <ul>Model: EcoBoostPremium / GT</ul>
+    <ul>Price: $32,495 / $37,545 </ul>
+    <ul>Gas Milage: 21City/30Highway / 15City/24Highway</ul>
     </div>
-    <div className="camry">
-        <h1>Toyota Camry</h1>
-        <ul>Model: TRD</ul>
-        <ul>Price: $32,910</ul>
-        <ul>Gas Milage: 21City/31Highway</ul>
+    <div className="mustangV8">
+    <h1> Ford Mustang</h1>
+    <ul>Model: GT</ul>
+    <ul>Price: $37,545 </ul>
+    <ul>Gas Milage: 15City/24Highway</ul>
+    </div>
+    <div className="subaru">
+        <h1>Subaru</h1>
+        <ul>Model: WRX Premium</ul>
+        <ul>Price: $34,155</ul>
+        <ul>Gas Milage: 19City/26Highway</ul>
+    </div>
+    <div className="dodgeV6">
+        <h1>Dodge Challenger</h1>
+        <ul>Model: GT </ul>
+        <ul>Price: $33,800 </ul>
+        <ul>Gas Milage: 19City/30Highway</ul>
+    </div>
+    <div className="dodgeV8">
+        <h1>Dodge Challenger</h1>
+        <ul>Model: RT</ul>
+        <ul>Price: $39,075</ul>
+        <ul>Gas Milage: 15City/23Highway</ul>
     </div>
     </div>
   );
